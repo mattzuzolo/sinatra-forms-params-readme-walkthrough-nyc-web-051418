bb7f449750dba2579882e8d9d5613df1f00a8a05
github username <github email address>
2018-06-04 19:48:19 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative 'config/environment'
 
 class App < Sinatra::Base
@@ -7,5 +8,9 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
+
+  post '/food' do
+      "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
 
 end
